01386f550c8b89464173980d46400ceec3a74aca
Atikur Rahman <atik.bd08@gmail.com>
2016-02-09 20:27:50 +0600

    format date

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,10 @@ module Listable
   def format_description(description)
     "#{description}".ljust(25)
   end
+  def format_date(options={})
+    dates = options[:start_date].strftime("%D") if options[:start_date]
+    dates << " -- " + options[:end_date].strftime("%D") if options[:end_date]
+    dates = "N/A" if !dates
+    return dates
+  end
 end
