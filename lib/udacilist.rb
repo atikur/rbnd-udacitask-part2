9dafd0db26bb8432551266d566d324cccaf5a5f2
Atikur Rahman <atik.bd08@gmail.com>
2016-02-09 21:02:04 +0600

    add error classes and error handling

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -10,8 +10,20 @@ class UdaciList
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
+
+    if !(type == "todo" || type == "event" || type == "link")
+      raise UdaciListErrors::InvalidItemType, "Invalid item type: #{type}"
+    end
+
+    priority = options[:priority]
+    if priority && !(priority == "high" || priority == "medium" || priority == "low")
+      raise UdaciListErrors::InvalidPriorityValue, "Invalid priority value #{priority}"
+    end
   end
   def delete(index)
+    if index > @items.count
+      raise UdaciListErrors::IndexExceedsListSize, "Item at index #{index} doesn't exist."
+    end
     @items.delete_at(index - 1)
   end
   def all
